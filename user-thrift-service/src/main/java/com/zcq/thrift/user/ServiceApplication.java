23d980abf8bcb2f29a78d1d118efad9a241c5203
zcq960921 <zcq960921@163.com>
2020-05-04 18:35:13 +0800

    thrift实现userservice

---
+++ user-thrift-service/src/main/java/com/zcq/thrift/user/ServiceApplication.java
@@ -0,0 +1,16 @@
+package com.zcq.thrift.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: chaoqun
+ * @Date: 2020/5/4 16:42
+ */
+@SpringBootApplication
+public class ServiceApplication {
+
+    public static void main(String args[]) {
+        SpringApplication.run(ServiceApplication.class, args);
+    }
+}
---
+++ user-thrift-service/src/main/java/com/zcq/thrift/user/service/UserSerivceImpl.java
@@ -0,0 +1,38 @@
+package com.zcq.thrift.user.service;
+
+
+import com.zcq.thrift.user.UserInfo;
+import com.zcq.thrift.user.UserService;
+import com.zcq.thrift.user.mapper.UserMapper;
+import org.apache.thrift.TException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserSerivceImpl implements UserService.Iface {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public UserInfo getUserById(int id) throws TException {
+
+        return userMapper.getUserById(id);
+    }
+
+    @Override
+    public UserInfo getTeacherById(int id) throws TException {
+        return null;
+    }
+
+
+    @Override
+    public UserInfo getUserByName(String username) throws TException {
+        return userMapper.getUserByName(username);
+    }
+
+    @Override
+    public void regiserUser(UserInfo userInfo) throws TException {
+        userMapper.registerUser(userInfo);
+    }
+}
